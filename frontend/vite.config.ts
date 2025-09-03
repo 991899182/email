ccf0397a539b211bacc7b68723a1c48006d3ef6a
991899182 <59867309+991899182@users.noreply.github.com>
2025-09-04 03:22:03 +0800

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://api.mail.mdzz.uk',
+        target: 'http://0tg.shop',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path,
